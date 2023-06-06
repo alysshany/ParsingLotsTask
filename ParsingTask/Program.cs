a94a032af27cdaf798fee9ddea4033c461788889
Zilya <galievaaziz@gmail.com>
2023-06-06 10:47:17 +0300

    updated + scroll

--- ParsingTask/Program.cs
+++ ParsingTask/Program.cs
@@ -48,6 +48,37 @@ namespace ConsoleApp
 
             driver.Navigate().GoToUrl("https://torgi.gov.ru/new/public/lots/reg");
 
+
+
+            //IList<IWebElement> showMoreButtons = driver.FindElements(By.XPath("//*[contains(text(), 'Показать еще')]"));
+
+            //// Нажимаем кнопки, пока они не исчезнут
+            //while (showMoreButtons.Count > 0)
+            //{
+            //    foreach (IWebElement button in showMoreButtons)
+            //    {
+            //        try
+            //        {
+            //            button.Click();
+            //        }
+            //        catch (Exception)
+            //        {
+            //            // Игнорируем исключения, если кнопка больше не найдена
+            //        }
+            //    }
+            //    showMoreButtons = driver.FindElements(By.XPath("//*[contains(text(), 'Показать еще')]"));
+            //}
+
+            // Находим высоту страницы
+            IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
+            long scrollHeight = (long)js.ExecuteScript("return Math.max( document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight );");
+
+            // Прокручиваем страницу до конца
+            for (int i = 0; i < scrollHeight; i += 100)
+            {
+                js.ExecuteScript($"window.scrollTo(0, {i});");
+            }
+
             //Скачиваем все ссыылки на лоты с страницы
             WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(80));
             wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementExists(By.CssSelector("a[class='lotLink'][target='_blank']")));
@@ -69,8 +100,6 @@ namespace ConsoleApp
 
                 var lotId = listOfLinks[i].Split('/')[7];
                 var lotTitle = driver.FindElement(By.CssSelector(".header_title")).Text;
-                var lotsInfo = driver.FindElements(By.CssSelector("div[class='lotAttributeValue']")).ToList();
-                var lotsInfoSecond = driver.FindElements(By.CssSelector("div[class='attr_value']")).ToList();
                 var lotsInfoPrices = driver.FindElements(By.CssSelector("div[class='prices__row__price-cell lotPrice']")).ToList();
                 var category = driver.FindElement(By.CssSelector("app-entity-attribute-simple[attributename='Категория объекта']")).FindElement(By.CssSelector("div[class='attr_value']")).Text.Trim();
 
@@ -88,19 +117,19 @@ namespace ConsoleApp
                     }
 
                     //Вид торгов
-                    var lotType = lotsInfo[1].Text;
+                    var lotType = ((driver.FindElement(By.XPath("//*[contains(text(), ' Вид торгов ')]"))).FindElement(By.XPath(".."))).FindElement(By.CssSelector("div[class='lotAttributeValue']")).Text.Trim();
                     //Предмет торгов(наименование лота)
-                    var lotSub = lotsInfoSecond[0].Text;
+                    var lotSub = ((driver.FindElement(By.XPath("//*[contains(text(), ' Предмет торгов (наименование лота) ')]"))).FindElement(By.XPath(".."))).FindElement(By.CssSelector("div[class='attr_value']")).Text.Trim();
                     //Описание лота
                     var lotDescription = driver.FindElement(By.CssSelector("app-entity-attribute-simple[attributename='Описание лота']")).FindElement(By.CssSelector("div[class='attr_value']")).Text.Trim();
                     //Субъект местонахождения имущества
-                    var locationSubject = lotsInfo[3].Text;
+                    var locationSubject = ((driver.FindElement(By.XPath("//*[contains(text(), ' Субъект местонахождения имущества ')]"))).FindElement(By.XPath(".."))).FindElement(By.CssSelector("div[class='lotAttributeValue']")).Text.Trim();
                     //Местонахождение имущества
                     var location = driver.FindElement(By.CssSelector("app-entity-attribute-simple[attributename='Местонахождение имущества']")).FindElement(By.CssSelector("div[class='attr_value']")).Text.Trim();
                     //Категория объекта
                     var objectCategory = driver.FindElement(By.CssSelector("app-entity-attribute-simple[attributename='Категория объекта']")).FindElement(By.CssSelector("div[class='attr_value']")).Text.Trim();
                     //Форма собственности
-                    var ownershipForm = lotsInfoSecond[7].Text; //??
+                    var ownershipForm = ((driver.FindElement(By.XPath("//*[contains(text(), ' Форма собственности ')]"))).FindElement(By.XPath(".."))).FindElement(By.CssSelector("div[class='attr_value']")).Text.Trim();
                     //Цель предоставления земельного участка
                     //Нет у Земли сельскохозяйственного назначения, Земли населенных пунктов, Земли специального назначения, Земли лесного фонда, Земли водного фонда, Земельные участки (категория не установлена)
                     //Есть у Земли особо охраняемых территорий и объектов, Земельные участки (не образованы)
@@ -108,9 +137,8 @@ namespace ConsoleApp
 
                     if (listOfCategoriesThatHavePurpose.Contains(category))
                     {
-                        landPurpose = lotsInfoSecond[8].Text;
+                        landPurpose = ((driver.FindElement(By.XPath("//*[contains(text(), ' Цель предоставления земельного участка ')]"))).FindElement(By.XPath(".."))).FindElement(By.CssSelector("div[class='attr_value']")).Text.Trim();
                     }
-
 
                     string lotDirectory = baseDirectory + lotId;
 
@@ -154,7 +182,6 @@ namespace ConsoleApp
                 }
 
             }
-            //}
 
             // закрытие браузера
             driver.Quit();
